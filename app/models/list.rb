f4d0b0ccb5f0b5607edbbcd0153d278ba278fd2d
EC2 Default User <ec2-user@ip-172-31-11-152.ap-northeast-1.compute.internal>
2023-11-05 14:34:42 +0000

    [fix]validates

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,7 @@
 class List < ApplicationRecord
   has_one_attached :image
 
-  validate :title, presence: true
-  validate :body, presence: true
-  validate :image, presence: true
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :image, presence: true
 end
